7add3274d764d3c83b1eef6c53329c6bfb0345af
Olivier Dagenais <olivier.dagenais@gmail.com>
2016-12-10 23:14:54 -0500

    Implement fade on R, G and B LEDs
    Most of this code was adapted from the Fade tutorial:
    https://www.arduino.cc/en/Tutorial/Fade

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,8 +1,41 @@
 // Copyright (c) Olivier Dagenais. All rights reserved.
 // Licensed under the MIT license. See LICENSE in the project root.
 
+/*
+ * scrappy-01: RGB pulse
+ *
+ * TODO: include diagram
+ */
+
+int inputRed = 2;
+int outputRed = 3;
+int inputBlue = 4;
+int outputBlue = 5;
+int outputGreen = 6;
+int inputGreen = 7;
+
+int brightness = 0;
+int fadeAmount = 5;
+
 void setup() {
+  pinMode(inputRed, INPUT);
+  pinMode(outputRed, OUTPUT);
+  pinMode(inputBlue, INPUT);
+  pinMode(outputBlue, OUTPUT);
+  pinMode(outputGreen, OUTPUT);
+  pinMode(inputGreen, INPUT);
 }
 
 void loop() {
+  analogWrite(outputRed, brightness);
+  analogWrite(outputBlue, brightness);
+  analogWrite(outputGreen, brightness);
+
+  brightness = brightness + fadeAmount;
+
+  if (brightness <= 0 || brightness >= 255) {
+    fadeAmount = -fadeAmount;
+  }
+
+  delay(30);
 }
